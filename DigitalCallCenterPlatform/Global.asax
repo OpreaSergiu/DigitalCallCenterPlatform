1deba17b6c15125ff6984d1e5f9b5ba91190fa66
Sergiu Oprea <sergiuandreioprea@gmail.com>
2018-10-31 15:02:40 +0200

    Initial commit of the project.

---
+++ DigitalCallCenterPlatform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DigitalCallCenterPlatform.MvcApplication" Language="C#" %>
---
+++ DigitalCallCenterPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalCallCenterPlatform.Startup))]
+namespace DigitalCallCenterPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
