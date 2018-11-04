d233aac6ed2dfe2ede341ba385ff2168f1227014
Sergiu Oprea <sergiuandreioprea@gmail.com>
2018-11-04 15:49:46 +0200

    Add Backoffice controller along wih the empty views for it. (Index, Trust, Newbusiness, PaymentRequest)

---
+++ DigitalCallCenterPlatform/Controllers/BackofficeController.cs
@@ -0,0 +1,34 @@
+﻿using DigitalCallCenterPlatform.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalCallCenterPlatform.Controllers
+{
+    public class BackofficeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewBusiness()
+        {
+            return View();
+        }
+
+        public ActionResult Trust()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentPost()
+        {
+            return View();
+        }
+    }
+}
