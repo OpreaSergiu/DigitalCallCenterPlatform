2b0f7cf1f4f079f849d657afa97c012d5578e5db
Sergiu Oprea <sergiuandreioprea@gmail.com>
2018-11-05 14:18:15 +0200

    Add ClientPortal controller along with it's views.

---
+++ DigitalCallCenterPlatform/Controllers/ClientPortalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitalCallCenterPlatform.Controllers
+{
+    public class ClientPortalController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        public ActionResult Payments()
+        {
+
+            return View();
+        }
+
+        public ActionResult Audit(int? id = 0)
+        {
+            return View();
+        }
+    }
+}
