5a7863d52492093032d9bb0ee22391024544e006
Sergiu Oprea <sergiuandreioprea@gmail.com>
2018-11-19 16:16:43 +0200

    Add payment handleing in both Backoffice and ClienPortal and finish the views for payments and Trust from backoffice.

--- DigitalCallCenterPlatform/Controllers/BackofficeController.cs
+++ DigitalCallCenterPlatform/Controllers/BackofficeController.cs
@@ -11,6 +11,8 @@ namespace DigitalCallCenterPlatform.Controllers
 {
     public class BackofficeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -21,14 +23,68 @@ namespace DigitalCallCenterPlatform.Controllers
             return View();
         }
 
-        public ActionResult Trust() 
+        public ActionResult Trust()
         {
             return View();
         }
 
         public ActionResult PaymentRequest()
         {
-            return View();
+            return View(db.PaymentsModels.ToList().Where(f => f.PostedFlag == false).OrderByDescending(d => d.PaymentDate));
+        }
+
+        public ActionResult PaymentPost(int id)
+        {
+            var PayModel = db.PaymentsModels.Find(id); ;
+            var AccModel = db.WorkPlatformModels.Find(PayModel.AccountNumber);
+
+            string NewInvoiceStatus = "OPEN";
+
+            float invouce_due = db.InvoiceModels.Where(m => m.AccountNumber == PayModel.AccountNumber).Where(m => m.Invoice == PayModel.Invoice).FirstOrDefault().Due;
+
+            if (invouce_due == PayModel.Amount)
+            {
+                NewInvoiceStatus = "PAID";
+            }
+
+            float NewInvoiceDue = invouce_due - PayModel.Amount;
+
+            string NewAccountStatus = AccModel.Status;
+
+            if (AccModel.TotalDue == PayModel.Amount)
+            {
+                NewAccountStatus = "CLOSED";
+            }
+
+            float NewTotalDue = AccModel.TotalDue - PayModel.Amount;
+            float NewTotalReceived = AccModel.TotalReceived + PayModel.Amount;
+
+            var invoice = db.InvoiceModels.Where(m => m.AccountNumber == PayModel.AccountNumber).Where(m => m.Invoice == PayModel.Invoice).FirstOrDefault();
+            if (invoice != null)
+            {
+                invoice.Due = NewInvoiceDue;
+                invoice.Status = NewInvoiceStatus;
+                invoice.PostedFlag = true;
+                db.SaveChanges();
+            }
+
+            var account = db.WorkPlatformModels.Where(m => m.Id == PayModel.AccountNumber).FirstOrDefault();
+            if (account != null)
+            {
+                account.TotalReceived = NewTotalReceived;
+                account.TotalDue = NewTotalDue;
+                account.Status = NewAccountStatus;
+                db.SaveChanges();
+            }
+
+            var payment = db.PaymentsModels.Where(m => m.Id == id).FirstOrDefault();
+            if (payment != null)
+            {
+                payment.PostedFlag = true;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("PaymentRequest");
         }
     }
 }
--- DigitalCallCenterPlatform/Controllers/ClientPortalController.cs
+++ DigitalCallCenterPlatform/Controllers/ClientPortalController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DigitalCallCenterPlatform.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace DigitalCallCenterPlatform.Controllers
 {
     public class ClientPortalController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -20,8 +24,37 @@ namespace DigitalCallCenterPlatform.Controllers
 
         public ActionResult Payments()
         {
+            string user_name = User.Identity.GetUserName();
+            var user_client = db.UserClientidModels.SingleOrDefault(b => b.UserEmail == user_name);
 
-            return View();
+            return View(db.PaymentsModels.Where(m => m.ClientID == user_client.ClientId).OrderByDescending(s => s.Id).ToList());
+        }
+
+        public ActionResult ApprovePayment(int id)
+        {
+            var result = db.PaymentsModels.SingleOrDefault(b => b.Id == id);
+            if (result != null)
+            {
+                result.Approve = true;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Payments");
+        }
+
+        public ActionResult DeletePayment(int id)
+        {
+            var result = db.PaymentsModels.SingleOrDefault(b => b.Id == id);
+            var invoice = db.InvoiceModels.SingleOrDefault(i => i.Invoice == result.Invoice);
+
+            if (result != null)
+            {
+                invoice.PaymentRequestFlag = false;
+                db.PaymentsModels.Remove(result);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Payments");
         }
 
         public ActionResult Audit(int? id = 0)
